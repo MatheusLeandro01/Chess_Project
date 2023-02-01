66f600212936196782a55193599b1820681f58cb
Matheus Leandro <matheus.leandroyt@gmail.com>
2023-02-01 09:40:24 -0300

    Implementando o tabuleiro

---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,18 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] Pecas;
+
+        public Tabuleiro() { }
+
+        public Tabuleiro(int linha, int coluna)
+        {
+            Linhas = linha;
+            Colunas = coluna; 
+            Pecas = new Peca[linha, coluna];
+        }
+    }
+}
